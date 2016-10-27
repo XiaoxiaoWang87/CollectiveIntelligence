2693b03ca925c441fa9654a07c900655dccd62df
Xiaoxiao Wang <wangxiao420@gmail.com>
2016-10-26 22:39:54 -0700

    add people

--- chapter5-optimization/optimization.py
+++ chapter5-optimization/optimization.py
@@ -1,1 +1,14 @@
 import time
+import random
+import math
+
+people = [ ('Seymour', 'BOS'), 
+           ('Franny', 'DAL'), 
+           ('Zooey', 'CAK'), 
+           ('Walt', 'MIA'), 
+           ('Buddy', 'ORD'), 
+           ('Les', 'OMA')
+         ]
+
+# LaGuardia airport in New York
+destination='LGA'
