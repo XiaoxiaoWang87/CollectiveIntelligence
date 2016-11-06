79130f66d4d17e3602ba6506a55c515d66cf64ee
Xiaoxiao Wang <wangxiao420@gmail.com>
2016-11-06 00:48:59 -0700

    chapter recommender

--- chapter2-recommendation/test.py
+++ chapter2-recommendation/test.py
@@ -6,3 +6,9 @@ print recommendations.getRecommendations(recommendations.critics,'Toby', similar
 movies=recommendations.transformPrefs(recommendations.critics)
 print recommendations.topMatches(movies,'Superman Returns')
 print recommendations.getRecommendations(movies,'Just My Luck')
+
+itemsim=recommendations.calculateSimilarItems(recommendations.critics)
+print itemsim
+
+
+print recommendations.getRecommendedItems(recommendations.critics,itemsim,'Toby')
